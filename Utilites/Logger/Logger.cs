d943859445826fb4708db4bd2476b4546b9f6cb3
Vlad-00003 <Vlad-00003@mail.ru>
2018-03-15 12:38:08 +0300

    Added extension method to see if the enum has the value. This is done cos the project is build via .Net 3.5 and I have no Enum.HasFlag =(

--- Utilites/Logger/Logger.cs
+++ Utilites/Logger/Logger.cs
@@ -190,6 +190,19 @@ namespace Utilites.Logger
         #endregion
 
         #region Helpers
+        /// <summary>
+        /// Checks if the enum containes value. This is .net 3.5 project, so there is no Enum.HasFlag =(
+        /// </summary>
+        /// <param name="keys"></param>
+        /// <param name="flag"></param>
+        /// <returns></returns>
+        public static bool Contains(this Enum keys, Enum flag)
+        {
+            ulong keysVal = Convert.ToUInt64(keys);
+            ulong flagVal = Convert.ToUInt64(flag);
+
+            return (keysVal & flagVal) == flagVal;
+        }
         private static void AddToFile(string assemblyName, string text)
         {
             var path = string.Format(Logpath, assemblyName);
